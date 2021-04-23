0dca7030ffefc754f69c5c83523434014032613b
Mikhail Kokaulin <work-dev@inforsys.ru>
2021-04-23 20:11:35 +0300

    update dependencies

--- src/work/core.clj
+++ src/work/core.clj
@@ -1,4 +1,10 @@
-(ns work.core)
+(ns work.core
+  (:require [jdk.awt.SystemTray :as SystemTray]
+            [jdk.awt.TrayIcon   :as TrayIcon]
+            [jdk.awt.Toolkit    :as Toolkit]
+            [jdk.awt.PopupMenu  :as PopupMenu]
+            [jdk.awt.TrayIcon$MessageType :refer [ERROR WARNING INFO]])
+  (:gen-class))
 
 
 (defn -main [& args]
